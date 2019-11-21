4f8fe459a0daeb966b92b352f97fe44547cb9121
VC <wht441465797@163.com>
2019-11-21 23:27:21 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import list from '@/components/list'
 import add from '@/components/add'
 import complete from '@/components/complete'
